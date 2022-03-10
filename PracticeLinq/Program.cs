272b6243b73d382da1bdc7cbf826ddfda4bfcdd1
Matthew Bonner <matthewsbonner@comcast.net>
2022-03-09 20:26:39 -0500

    init commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new List<string>() { "Elden Ring", "Ghost of Tsushima", "Monster Hunter", "Final Fantasy", "Destiny 2" };
+            var gamesSorted = games.OrderBy(name => name.Length);
+            foreach(var game in gamesSorted)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
